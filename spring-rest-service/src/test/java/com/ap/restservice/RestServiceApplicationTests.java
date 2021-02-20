004fc0270ab604259fc23ee9982c29cb10b33abe
Hieu Nguyen <hieu.nguyen@wizeline.com>
2021-02-20 20:38:59 +0700

    init DB

--- spring-rest-service/src/test/java/com/ap/restservice/RestServiceApplicationTests.java
+++ spring-rest-service/src/test/java/com/ap/restservice/RestServiceApplicationTests.java
@@ -1,13 +1,15 @@
 package com.ap.restservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
